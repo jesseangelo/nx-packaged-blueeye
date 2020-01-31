f67ba158d5b0ac81a16cf8d85154be196fb79125
Jesse Angelo <jesserangelo@gmail.com>
2020-01-31 15:41:46 -0500

    reusable button and lib

--- apps/blueeye-platform/src/app/app.component.ts
+++ apps/blueeye-platform/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OneLib } from '@jetblue/one-lib';
 
 @Component({
   selector: 'jetblue-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blueeye-platform';
+  business: OneLib = new OneLib();
 }
--- apps/blueeye-platform/src/app/app.module.ts
+++ apps/blueeye-platform/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TwoLibModule } from '@jetblue/two-lib';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, TwoLibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
